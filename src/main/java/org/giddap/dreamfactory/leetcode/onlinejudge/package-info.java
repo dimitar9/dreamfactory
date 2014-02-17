a3074c743a418148768b2fa06aadf8127b8a68ec
b1m4w3n <b1m4w3n@gmail.com>
2014-02-16 18:29:37 -0800

    Add package-info.java

---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * b1m4w3n's solutions to the coding challenges at leetcode.com.
+ * <p>
+ * <ul>
+ * <li>
+ * Maximum subarray: 'best to buy and sell stocks III'
+ * </li>
+ * <li>
+ * Level-by-level BST to find depth: 'Minimum/Maximum depth of binary trees',
+ * 'word ladder'.
+ * </li>
+ *
+ * </ul>
+ *
+ * @since 1.0
+ * @author b1m4w3n
+ * @version 1.0
+ */
+package org.giddap.dreamfactory.leetcode.onlinejudge;
