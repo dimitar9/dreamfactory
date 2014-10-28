db98e1e2bbd2ca14b7465128f6cbe4fd14260006
b1m4w3n <b1m4w3n@gmail.com>
2014-10-27 23:21:52 -0400

    leetcode clone graph

---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/CloneGraphBfsRecursiveImpl.java
@@ -0,0 +1,46 @@
+package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
+
+import org.giddap.dreamfactory.leetcode.commons.UndirectedGraphNode;
+import org.giddap.dreamfactory.leetcode.onlinejudge.CloneGraph;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ *
+ */
+public class CloneGraphBfsRecursiveImpl implements CloneGraph {
+    @Override
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        Queue<UndirectedGraphNode> frontier = new LinkedList<UndirectedGraphNode>();
+        frontier.offer(node);
+        Map<UndirectedGraphNode, UndirectedGraphNode> visited =
+                new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        UndirectedGraphNode cloneNode = new UndirectedGraphNode(node.label);
+        visited.put(node, cloneNode);
+
+        while (!frontier.isEmpty()) {
+            UndirectedGraphNode curr = frontier.poll();
+            cloneNode = visited.get(curr);
+
+            for (UndirectedGraphNode neighbor : curr.neighbors) {
+                UndirectedGraphNode cloneNeighbor;
+                if (visited.containsKey(neighbor)) {
+                    cloneNeighbor = visited.get(neighbor);
+                } else {
+                    cloneNeighbor = new UndirectedGraphNode(neighbor.label);
+                    visited.put(neighbor, cloneNeighbor);
+                    frontier.offer(neighbor);
+                }
+                cloneNode.neighbors.add(cloneNeighbor);
+            }
+        }
+        return visited.get(node);
+    }
+}
