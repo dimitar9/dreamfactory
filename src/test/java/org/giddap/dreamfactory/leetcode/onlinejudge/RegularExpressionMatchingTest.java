32d0b786d45719f87ae4153f5734b950a309fc5c
b1m4w3n <b1m4w3n@gmail.com>
2014-11-02 22:25:28 -0500

    leetcode regular expression matching

--- src/test/java/org/giddap/dreamfactory/leetcode/onlinejudge/RegularExpressionMatchingTest.java
+++ src/test/java/org/giddap/dreamfactory/leetcode/onlinejudge/RegularExpressionMatchingTest.java
@@ -1,12 +1,15 @@
 package org.giddap.dreamfactory.leetcode.onlinejudge;
 
+import org.giddap.dreamfactory.leetcode.onlinejudge.implementations.RegularExpressionMatchingBacktrackingImpl;
 import org.giddap.dreamfactory.leetcode.onlinejudge.implementations.RegularExpressionMatchingDPImpl;
+import org.giddap.dreamfactory.leetcode.onlinejudge.implementations.RegularExpressionMatchingRecursionImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RegularExpressionMatchingTest {
-    //    RegularExpressionMatching rem = new RegularExpressionMatchingBacktrackingImpl();
-    RegularExpressionMatching rem = new RegularExpressionMatchingDPImpl();
+    RegularExpressionMatching rem = new RegularExpressionMatchingBacktrackingImpl();
+    RegularExpressionMatching rem2 = new RegularExpressionMatchingDPImpl();
+    RegularExpressionMatching rem1 = new RegularExpressionMatchingRecursionImpl();
 
     @Test
     public void small01() {
@@ -41,5 +44,20 @@ public class RegularExpressionMatchingTest {
     @Test
     public void small07() {
         Assert.assertTrue(rem.isMatch("aab", "c*a*b"));
+    }
+
+    @Test
+    public void small08() {
+        Assert.assertFalse(rem.isMatch("a", ".*..a*"));
+    }
+
+    @Test
+    public void small09() {
+        Assert.assertTrue(rem.isMatch("aaa", "a*a"));
+    }
+
+    @Test
+    public void small10() {
+        Assert.assertFalse(rem.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c"));
     }
 }
