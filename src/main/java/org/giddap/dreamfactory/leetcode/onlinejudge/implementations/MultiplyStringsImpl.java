72fad2e7316fba681375197a5f69f94386c3e2cd
b1m4w3n <b1m4w3n@gmail.com>
2014-11-07 00:21:02 -0500

    leetcode multiply strings

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/MultiplyStringsImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/MultiplyStringsImpl.java
@@ -6,41 +6,41 @@ public class MultiplyStringsImpl implements MultiplyStrings {
 
     @Override
     public String multiply(String num1, String num2) {
-        final int len1 = num1.length();
-        final int len2 = num2.length();
-        if (len1 == 0 || len2 == 0) {
-            return "";
-        }
-        int[] ret = new int[len1 + len2];
+        char[] n1 = num1.toCharArray();
+        char[] n2 = num2.toCharArray();
 
-        for (int i = len1 - 1; i >= 0; i--) {
+        if (n1.length < n2.length) {
+            char[] tmp = n1;
+            n1 = n2;
+            n2 = tmp;
+        }
+
+        int[] m = new int[n1.length + n2.length];
+        for (int i = n2.length - 1; i >= 0; i--) {
+            int in22 = n2[i] - '0';
             int carry = 0;
-            int d1 = num1.charAt(i) - '0';
-            for (int j = len2 - 1; j >= 0; j--) {
-                int d2 = num2.charAt(j) - '0';
-                int sum = carry;
-                sum += d1 * d2;
-                sum += ret[i + j + 1];
-                ret[i + j + 1] = sum % 10;
-                carry = sum / 10;
+            for (int j = n1.length - 1; j >= 0; j--) {
+                int in11 = n1[j] - '0';
+                int one = in22 * in11 + carry + m[i + j + 1];
+                m[i + j + 1] = one % 10;
+                carry = one / 10;
             }
-
             if (carry != 0) {
-                ret[i] += carry;
+                m[i] += carry;
             }
         }
 
-        StringBuilder num = new StringBuilder();
-        boolean leadingZero = true;
-        for (int i = 0; i < ret.length; i++) {
-            if (leadingZero && ret[i] == 0) {
-                continue;
-            } else {
-                num.append(ret[i]);
-                leadingZero = false;
+        StringBuilder ret = new StringBuilder();
+        boolean nonzero = false;
+        for (int i : m) {
+            if (i != 0 && !nonzero) {
+                nonzero = true;
             }
-        }
+            if (nonzero) {
+                ret.append(i);
+            }
 
-        return num.length() == 0 ? "0" : num.toString();
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
     }
 }
