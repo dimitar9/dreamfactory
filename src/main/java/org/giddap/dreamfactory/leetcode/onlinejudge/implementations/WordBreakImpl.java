4d6d58afccdcd3333d98cc09754cf8112977a583
b1m4w3n <b1m4w3n@gmail.com>
2014-11-19 22:44:17 -0500

    leetcode word break

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/WordBreakImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/WordBreakImpl.java
@@ -11,19 +11,20 @@ public class WordBreakImpl implements WordBreak {
     public boolean wordBreak(String s, Set<String> dict) {
         final int len = s.length();
         if (len == 0) {
-            return true;
+            return false;
         }
 
-        boolean[] breakables = new boolean[len + 1];
-        breakables[0] = true;
-        for (int i = 0; i < len; i++) {
+        boolean[] memo = new boolean[len + 1];
+        memo[0] = true;
+        for (int i = 1; i <= len; i++) {
             for (int j = 0; j <= i; j++) {
-                if (breakables[j] && dict.contains(s.substring(j, i + 1))) {
-                    breakables[i + 1] = true;
+                if (memo[j] && dict.contains(s.substring(j, i))) {
+                    memo[i] = true;
                     break;
                 }
             }
         }
-        return breakables[len];
+
+        return memo[len];
     }
 }
