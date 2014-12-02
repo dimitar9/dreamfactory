ea78b70583124d7a2035c707d7d38d9b8ee4444b
b1m4w3n <b1m4w3n@gmail.com>
2014-12-01 23:11:07 -0500

    leetcode sudoku solver refactoring

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SudokuSolver.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SudokuSolver.java
@@ -9,10 +9,6 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
  * http://www.ocf.berkeley.edu/~jchu/publicportal/sudoku/sudoku.paper.html
  * http://en.wikipedia.org/wiki/Dancing_Links
  */
-public class Q037SudokuSolver {
-    public void solveSudoku(char[][] board) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-
-    }
+public interface SudokuSolver {
+    void solveSudoku(char[][] board);
 }
---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/SudokuSolverImpl.java
@@ -0,0 +1,13 @@
+package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
+
+import org.giddap.dreamfactory.leetcode.onlinejudge.SudokuSolver;
+
+/**
+ * Created by peng on 12/1/14.
+ */
+public class SudokuSolverImpl implements SudokuSolver {
+    @Override
+    public void solveSudoku(char[][] board) {
+        
+    }
+}
