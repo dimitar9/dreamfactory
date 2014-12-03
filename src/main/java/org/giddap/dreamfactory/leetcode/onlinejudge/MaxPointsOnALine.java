ef4f94eb53264b601006de5f75b3ea625d4fdbc7
b1m4w3n <b1m4w3n@gmail.com>
2014-12-03 17:56:11 -0500

    leetcode max point on a line.

---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/MaxPointsOnALine.java
@@ -0,0 +1,10 @@
+package org.giddap.dreamfactory.leetcode.onlinejudge;
+
+import org.giddap.dreamfactory.leetcode.commons.Point;
+
+/**
+ *
+ */
+public interface MaxPointsOnALine {
+    int maxPoints(Point[] points);
+}
---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/MaxPointsOnALineImpl.java
@@ -0,0 +1,14 @@
+package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
+
+import org.giddap.dreamfactory.leetcode.commons.Point;
+import org.giddap.dreamfactory.leetcode.onlinejudge.MaxPointsOnALine;
+
+/**
+ *
+ */
+public class MaxPointsOnALineImpl implements MaxPointsOnALine {
+    @Override
+    public int maxPoints(Point[] points) {
+        return 0;
+    }
+}
