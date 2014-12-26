1de440db2facf55c15090b2c5de44b146d49a9a4
b1m4w3n <b1m4w3n@gmail.com>
2014-12-26 09:30:17 -0500

    geeksforgeeks first non-repeated character refactoring

--- src/main/java/org/giddap/dreamfactory/geeksforgeeks/FirstNonRepeatedCharacter.java
+++ src/main/java/org/giddap/dreamfactory/geeksforgeeks/FirstNonRepeatedCharacter.java
@@ -5,6 +5,6 @@ package org.giddap.dreamfactory.geeksforgeeks;
  */
 public class FirstNonRepeatedCharacter {
     public char findFirstNonRepeatedCharacter(final String word) {
-
+        return 'a';
     }
 }
