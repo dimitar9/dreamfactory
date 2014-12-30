4dd3fe769dc455325b281efda3aec5ca6606c3fc
b1m4w3n <b1m4w3n@gmail.com>
2014-12-29 22:50:48 -0500

    leetcode - two sum

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/TwoSumImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/TwoSumImpl.java
@@ -10,15 +10,18 @@ public class TwoSumImpl implements TwoSum {
 
     @Override
     public int[] twoSum(int[] numbers, int target) {
-        Map<Integer, Integer> delta = new HashMap<Integer, Integer>();
-        // Iterating through the array to find the delta
         int[] ret = new int[2];
+        Map<Integer, Integer> visited = new HashMap<>();
         for (int i = 0; i < numbers.length; i++) {
-            if (delta.containsKey(numbers[i])) {
-                ret[0] = Math.min(i, delta.get(numbers[i])) + 1;
-                ret[1] = Math.max(i, delta.get(numbers[i])) + 1;
+            int delta = target - numbers[i];
+            if (visited.containsKey(delta)) {
+                ret[0] = visited.get(delta) + 1;
+                ret[1] = i + 1;
+                return ret;
             } else {
-                delta.put(target - numbers[i], i);
+                if (!visited.containsKey(numbers[i])) {
+                    visited.put(numbers[i], i);
+                }
             }
         }
         return ret;
