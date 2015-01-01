ad5eb85f054980444fc05d3b301cbf7d2f93171b
b1m4w3n <b1m4w3n@gmail.com>
2015-01-01 18:21:08 -0500

    leetcode - generate parentheses

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/GenerateParenthesisImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/GenerateParenthesisImpl.java
@@ -8,23 +8,22 @@ import java.util.List;
 public class GenerateParenthesisImpl implements GenerateParenthesis {
     @Override
     public List<String> generateParenthesis(int n) {
-        List<String> ret = new ArrayList<String>();
-        build(ret, "", n, n);
+        List<String> ret = new ArrayList<>();
+        generate(ret, "", n, n);
         return ret;
     }
 
-    private void build(List<String> ret, String curr, int remainOpen, int remainClose) {
-        if (remainOpen == 0 && remainClose == 0) {
+    private void generate(List<String> ret, String curr, int open, int close) {
+        if (open == 0 && close == 0) {
             ret.add(curr);
-            return;
-        }
+        } else {
+            if (open > 0) {
+                generate(ret, curr + "(", open - 1, close);
+            }
 
-        if (remainOpen > 0) {
-            build(ret, curr + "(", remainOpen - 1, remainClose);
-        }
-
-        if (remainOpen < remainClose) {
-            build(ret, curr + ")", remainOpen, remainClose - 1);
+            if (open < close) {
+                generate(ret, curr + ")", open, close - 1);
+            }
         }
     }
 }
