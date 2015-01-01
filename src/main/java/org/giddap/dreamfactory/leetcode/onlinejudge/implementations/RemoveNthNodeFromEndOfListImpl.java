150c82ba99d803e95950b0eaeb66fbfd7e3b6c5b
b1m4w3n <b1m4w3n@gmail.com>
2015-01-01 16:22:07 -0500

    leetcode - remove nth node from end of list

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/RemoveNthNodeFromEndOfListImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/RemoveNthNodeFromEndOfListImpl.java
@@ -6,26 +6,27 @@ import org.giddap.dreamfactory.leetcode.onlinejudge.RemoveNthNodeFromEndOfList;
 public class RemoveNthNodeFromEndOfListImpl implements RemoveNthNodeFromEndOfList {
     @Override
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode newhead = new ListNode(0);
-        newhead.next = head;
-
+        // advance the 'fast' pointer to 'nth' node
         ListNode fast = head;
         int i = 1;
         while (i < n && fast != null) {
             fast = fast.next;
             i++;
         }
-
-        ListNode slow = head;
+        // boundary condition check
+        if (fast == null) {
+            return head;
+        }
+        // advance both pointers until 'fast' reaches the end
+        ListNode newhead = new ListNode(0);
+        newhead.next = head;
         ListNode prev = newhead;
         while (fast != null && fast.next != null) {
-            prev = slow;
-            slow = slow.next;
             fast = fast.next;
+            prev = prev.next;
         }
-
-        prev.next = slow.next;
-        slow.next = null;
+        // remove the 'n-th' node
+        prev.next = prev.next.next;
         return newhead.next;
     }
 }
