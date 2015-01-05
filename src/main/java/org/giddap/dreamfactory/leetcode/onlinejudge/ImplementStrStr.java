baaae828b024a7cbd5303f954b61adafcb5b7e97
b1m4w3n <b1m4w3n@gmail.com>
2015-01-04 19:09:29 -0500

    leetcode - implement strstr

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/ImplementStrStr.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/ImplementStrStr.java
@@ -11,6 +11,9 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
  * <p/>
  *
  * @see <a href="http://leetcode.com/2010/10/implement-strstr-to-find-substring-in.html">Leetcode blog</a>
+ * KMP: http://blog.csdn.net/v_july_v/article/details/7041827
+ * KMP: http://blog.csdn.net/yaochunnian/article/details/7059486
+ * KMP: http://billhoo.blog.51cto.com/2337751/411486/
  */
 public interface ImplementStrStr {
     int strStr(String haystack, String needle);
