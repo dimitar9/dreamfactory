87f4ab979ff1150599149139700b3f9f24c9b704
b1m4w3n <b1m4w3n@gmail.com>
2015-01-13 22:39:56 -0500

    leetcode - rotate image

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/RotateImageImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/RotateImageImpl.java
@@ -7,23 +7,22 @@ public class RotateImageImpl implements RotateImage {
     @Override
     public void rotate(int[][] matrix) {
         final int n = matrix.length;
-        int layer = 0;
-        while (2 * layer <= n) {
-            int len = n - 2 * layer - 1;
-            for (int i = 0; i < len; i++) {
-                // top to right
-                int right = matrix[layer + i][layer + len];
-                matrix[layer + i][layer + len] = matrix[layer][layer + i];
-                // right to bottom
-                int bottom = matrix[layer + len][layer + len - i];
-                matrix[layer + len][layer + len - i] = right;
-                // bottom to left
-                int left = matrix[layer + len - i][layer];
-                matrix[layer + len - i][layer] = bottom;
-                // left to top
-                matrix[layer][layer + i] = left;
+        final int layers = n / 2;
+        for (int i = 0; i < layers; i++) {
+            int m = n - 2 * i - 2;
+            for (int j = 0; j <= m; j++) {
+                int tmp = matrix[i + j][i + m + 1];
+                matrix[i + j][i + m + 1] = matrix[i][i + j];
+                matrix[i][i + j] = tmp;
+
+                tmp = matrix[i + m + 1][i + m + 1 - j];
+                matrix[i + m + 1][i + m + 1 - j] = matrix[i][i + j];
+                matrix[i][i + j] = tmp;
+
+                tmp = matrix[i + m + 1 -j][i];
+                matrix[i + m + 1 -j][i] = matrix[i][i + j];
+                matrix[i][i + j] = tmp;
             }
-            layer++;
         }
     }
 }
