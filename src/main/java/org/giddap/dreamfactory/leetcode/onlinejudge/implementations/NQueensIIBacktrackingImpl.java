2f8874d6f5cc27e806c6de8f60e65dc01822c79c
b1m4w3n <b1m4w3n@gmail.com>
2015-01-17 11:41:27 -0500

    leetcode - n queens ii

---
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/NQueensIIBacktrackingImpl.java
@@ -0,0 +1,56 @@
+package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
+
+import org.giddap.dreamfactory.leetcode.onlinejudge.NQueensII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class NQueensIIBacktrackingImpl implements NQueensII {
+
+    @Override
+    public int totalNQueens(int n) {
+        List<String[]> res = new ArrayList<>();
+        buildConfiuration(0, new int[n], res);
+        return res.size();
+    }
+
+    private void buildConfiuration(
+            int row, int[] cols, List<String[]>res) {
+        final int n = cols.length;
+        if (row == n) {
+            String[] item = new String[n];
+            for (int i = 0; i < n; i++) {
+                StringBuilder strRow = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    if (cols[i] == j)
+                        strRow.append('Q');
+                    else
+                        strRow.append('.');
+                }
+                item[i] = strRow.toString();
+            }
+            res.add(item);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            cols[row] = i;
+            if (isValidConfiguration(row, cols)) {
+                buildConfiuration(row + 1, cols, res);
+            }
+        }
+    }
+
+    private boolean isValidConfiguration(int row, int[] cols) {
+        for (int i = 0; i < row; i++) {
+            if (cols[row] == cols[i]
+                    || Math.abs(cols[row] - cols[i]) == row - i) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
