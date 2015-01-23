5c4662a329da13b2489ef4198059015d849348f2
b1m4w3n <b1m4w3n@gmail.com>
2015-01-22 22:51:49 -0500

    leetcode - plus one

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/PlusOneImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/PlusOneImpl.java
@@ -8,22 +8,22 @@ import java.util.Arrays;
 public class PlusOneImpl implements PlusOne {
     @Override
     public int[] plusOne(int[] digits) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        final int len = digits.length;
-        int[] ret = new int[len + 1];
-        ret[len] = 1;
-        int carry = 0;
-        for (int i = len - 1; i >= 0; i--) {
-            int sum = ret[i + 1] + digits[i] + carry;
+        final int n = digits.length;
+        if (n == 0) {
+            return new int[0];
+        }
+        int[] ret = new int[n + 1];
+        int carry = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
             ret[i + 1] = sum % 10;
             carry = sum / 10;
         }
-        if (carry > 0) {
+        if (carry != 0) {
             ret[0] = carry;
             return ret;
         } else {
-            return Arrays.copyOfRange(ret, ret.length > 1 ? 1 : 0, len + 1);
+            return Arrays.copyOfRange(ret, 1, ret.length);
         }
     }
 }
