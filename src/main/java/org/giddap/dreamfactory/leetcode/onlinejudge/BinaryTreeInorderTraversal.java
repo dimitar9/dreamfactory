3aac864bfdf481c9e4b2a38a48d4ab87668a5fdc
b1m4w3n <b1m4w3n@gmail.com>
2015-02-06 23:45:53 -0500

    leetcode - binary tree inorder traversal

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BinaryTreeInorderTraversal.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BinaryTreeInorderTraversal.java
@@ -7,9 +7,6 @@ import java.util.List;
 /**
  * <a href="http://oj.leetcode.com/problems/binary-tree-inorder-traversal/">Binary Tree In-Order Traversal</a>
  * <p/>
- * Copyright 2013 LeetCode
- * <p/>
- * <p/>
  * Given a binary tree, return the in-order traversal of its nodes' values.
  * <p/>
  * <pre>
@@ -27,13 +24,6 @@ import java.util.List;
  * Note: Recursive solution is trivial, could you do it iteratively?
  * <p/>
  * confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
- * <p/>
- *
- * @see <a href="http://discuss.leetcode.com/questions/23/binary-tree-inorder-traversal">Leetcode discussion</a>
- * @see <a href="http://leetcode.com/2010/04/binary-search-tree-in-order-traversal.html">Leetcode blog</a>
- * @see <a href="http://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/">Geeksforgeeks blog</a>
- * http://en.wikipedia.org/wiki/Threaded_binary_tree
- * http://www.eternallyconfuzzled.com/tuts/datastructures/jsw_tut_bst1.aspx#thread
  */
 public interface BinaryTreeInorderTraversal {
     List<Integer> inorderTraversal(TreeNode root);
