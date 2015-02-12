a02aa801d5ffbd9de009e39b5055badbbc554d54
b1m4w3n <b1m4w3n@gmail.com>
2015-02-11 23:37:03 -0500

    leetcode - max depth of binary tree

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/MaximumDepthOfBinaryTree.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/MaximumDepthOfBinaryTree.java
@@ -5,9 +5,6 @@ import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 /**
  * <a href="http://oj.leetcode.com/problems/maximum-depth-of-binary-tree/">Maximum Depth of Binary Tree</a>
  * <p/>
- * Copyright 2013 LeetCode
- * http://leetcode.com/onlinejudge#question_104
- * <p/>
  * Given a binary tree, find its maximum depth.
  * <p/>
  * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/MaximumDepthOfBinaryTreeRecursiveImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/MaximumDepthOfBinaryTreeRecursiveImpl.java
@@ -3,13 +3,15 @@ package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 import org.giddap.dreamfactory.leetcode.onlinejudge.MaximumDepthOfBinaryTree;
 
+/**
+ * Recursive solution
+ */
 public class MaximumDepthOfBinaryTreeRecursiveImpl implements MaximumDepthOfBinaryTree {
     @Override
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
