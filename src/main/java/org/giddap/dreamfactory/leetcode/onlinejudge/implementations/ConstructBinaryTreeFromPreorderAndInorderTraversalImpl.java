89cad1f43de52a7c5bfdcd0496a821cd45c9d7a7
b1m4w3n <b1m4w3n@gmail.com>
2015-02-14 16:09:40 -0500

    leetcode - construct binary trees

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/ConstructBinaryTreeFromPreorderAndInorderTraversalImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/ConstructBinaryTreeFromPreorderAndInorderTraversalImpl.java
@@ -5,14 +5,21 @@ import org.giddap.dreamfactory.leetcode.onlinejudge.ConstructBinaryTreeFromPreor
 
 import java.util.Arrays;
 
-
-public class ConstructBinaryTreeFromPreorderAndInorderTraversalImpl implements ConstructBinaryTreeFromPreorderAndInorderTraversal {
+/**
+ * Pre-order: root - left - right
+ * In-order: left - root -right
+ *
+ * More efficient to use binary-search to find the matching index
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversalImpl
+        implements ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
     @Override
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         final int len = preorder.length;
         if (len != inorder.length) {
-            throw new IllegalArgumentException("both arrays should have the same length.");
+            throw new IllegalArgumentException(
+                    "both arrays should have the same length.");
         }
         if (len == 0) {
             return null;
@@ -25,9 +32,10 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversalImpl implements C
         }
 
         TreeNode root = new TreeNode(rootVal);
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1, i + 1), Arrays.copyOfRange(inorder, 0, i));
-        root.right = buildTree(Arrays.copyOfRange(preorder, i + 1, len), Arrays.copyOfRange(inorder, i + 1, len));
-
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, i + 1),
+                              Arrays.copyOfRange(inorder, 0, i));
+        root.right = buildTree(Arrays.copyOfRange(preorder, i + 1, len),
+                               Arrays.copyOfRange(inorder, i + 1, len));
         return root;
     }
 }
--- src/main/java/org/giddap/dreamfactory/mitbbs/FindDuplicate.java
+++ src/main/java/org/giddap/dreamfactory/mitbbs/FindDuplicate.java
@@ -1,7 +1,27 @@
 package org.giddap.dreamfactory.mitbbs;
 
 /**
- *
+ * Problem Description:
+ * If you have numbers in a 10,000 item array that range from 1 to 1 million,
+ * and there is one duplicate. How do you find the duplicate with the simplest
+ * algorithm?
  */
 public class FindDuplicate {
+    private final static int LENGTH = 10000;
+    private final static int SCALE = 100;
+    public int findDuplicate(int[] numbers) {
+        if (numbers.length != LENGTH) {
+            throw new IllegalArgumentException("Incorrect number of elements");
+        }
+
+        for (int i = 0; i < LENGTH; i++) {
+            int n = numbers[i];
+            if (n <= LENGTH) {
+
+            } else {
+
+            }
+        }
+        throw new IllegalStateException("");
+    }
 }
