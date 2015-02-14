7889135f3cb2ebebb46534b0d4572d2589285f4b
b1m4w3n <b1m4w3n@gmail.com>
2015-02-14 16:44:59 -0500

    leetcode - path sum

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/PathSum.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/PathSum.java
@@ -5,11 +5,8 @@ import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 /**
  * <a href="http://oj.leetcode.com/problems/path-sum/">Path Sum</a>
  * <p/>
- * Copyright 2013 LeetCode
- * <p/>
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values
  * along the path equals the given sum.
- * <p/>
  * <pre>
  * For example:
  * Given the below binary tree and sum = 22,
@@ -22,9 +19,6 @@ import org.giddap.dreamfactory.leetcode.commons.TreeNode;
  * 7    2      1
  * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
  * </pre>
- * <p/>
- *
- * @see <a href="http://discuss.leetcode.com/questions/278/path-sum">leetcode discussion</a>
  */
 public interface PathSum {
     boolean hasPathSum(TreeNode root, int sum);
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/PathSumRecursiveDfsImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/PathSumRecursiveDfsImpl.java
@@ -4,26 +4,11 @@ import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 import org.giddap.dreamfactory.leetcode.onlinejudge.PathSum;
 
 /**
- * 弼馬溫注解：
- * <ul>
- * <li>DFS.
- * <ul>
- * <li>Typical DFS</li>
- * </ul>
- * </li>
- * <li>
- * Time complexity: O(n) on both operations.
- * </li>
- * <li>
- * Space complexity: O(1).
- * </li>
- * </ul>
+ * DFS.
  */
 public class PathSumRecursiveDfsImpl implements PathSum {
     @Override
     public boolean hasPathSum(TreeNode root, int sum) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
         if (root == null) {
             return false;
         }
