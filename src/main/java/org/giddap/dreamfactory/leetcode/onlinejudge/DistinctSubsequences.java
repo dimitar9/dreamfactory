942d4c11838de0f23ce41474dfe9781a9d42a05e
b1m4w3n <b1m4w3n@gmail.com>
2015-02-15 23:06:05 -0500

    leetcode - flatten binary tree to linked list

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/DistinctSubsequences.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/DistinctSubsequences.java
@@ -3,9 +3,6 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
 /**
  * <a href="http://oj.leetcode.com/problems/distinct-subsequences/">Distinct Subsequences</a>
  * <p/>
- * Copyright 2013 LeetCode
- * <p/>
- * <p/>
  * Given a string S and a string T, count the number of distinct subsequences of T in S.
  * <p/>
  * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none)
@@ -16,10 +13,6 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
  * S = "rabbbit", T = "rabbit"
  * <p/>
  * Return 3.
- * <p/>
- *
- * @see <a href="http://tech-lightnight.blogspot.com/2012/11/distinct-subsequences.html">A recursive solution</a>
- * http://n00tc0d3r.blogspot.com/2013/02/find-number-of-distinct-subsequences.html
  */
 public interface DistinctSubsequences {
     int numDistinct(String S, String T);
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/DistinctSubsequencesRecursiveImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/DistinctSubsequencesRecursiveImpl.java
@@ -3,25 +3,24 @@ package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 import org.giddap.dreamfactory.leetcode.onlinejudge.DistinctSubsequences;
 
 /**
- *
+ * Backtracking, recursively.
+ * if (S[i] == T[i]), there is one more branch: match(S[i + 1], T[j + 1]).
+ * for all cases, match(S[i + 1], T[j]);
  */
 public class DistinctSubsequencesRecursiveImpl implements DistinctSubsequences {
     @Override
     public int numDistinct(String S, String T) {
-        if (T.length() == 0) {
-            return 1;
-        }
-        if (S.length() < T.length()) {
-            return 0;
-        } else if (S.length() == T.length()) {
-            return S.equals(T) ? 1 : 0;
+        final int m = S.length();
+        final int n = T.length();
+        if (m == 0 || n == 0) {
+            return n == 0 ? 1 : 0;
         } else {
-            int ret = 0;
+            int r = 0;
             if (S.charAt(0) == T.charAt(0)) {
-                ret += numDistinct(S.substring(1), T.substring(1));
+                r += numDistinct(S.substring(1), T.substring(1));
             }
-            ret += numDistinct(S.substring(1), T);
-            return ret;
+            r += numDistinct(S.substring(1), T);
+            return r;
         }
     }
 }
