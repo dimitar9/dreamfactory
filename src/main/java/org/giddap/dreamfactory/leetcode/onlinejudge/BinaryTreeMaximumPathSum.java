bdf508ca6a89f95d3c60a3a7f4bf81e2b952d959
b1m4w3n <b1m4w3n@gmail.com>
2015-02-17 16:48:46 -0500

    leetcode - buy and sell stock iii

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BinaryTreeMaximumPathSum.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BinaryTreeMaximumPathSum.java
@@ -3,14 +3,13 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
 import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 
 /**
- * <a href="http://oj.leetcode.com/problems/binary-tree-maximum-path-sum/">Binary Tree Maximum Path Sum</a>
- * <p/>
- * Copyright 2013 LeetCode
- * <p/>
+ * <a href="http://oj.leetcode.com/problems/binary-tree-maximum-path-sum/">
+ * Binary Tree Maximum Path Sum</a>
+ * <p>
  * Given a binary tree, find the maximum path sum.
- * <p/>
+ * <p>
  * The path may start and end at any node in the tree.
- * <p/>
+ * <p>
  * <pre>
  * For example:
  * Given the below binary tree,
@@ -20,9 +19,6 @@ import org.giddap.dreamfactory.leetcode.commons.TreeNode;
  *   2   3
  * Return 6.
  * </pre>
- * <p/>
- *
- * http://n00tc0d3r.blogspot.com/2013/01/tree-path-sum.html
  */
 public interface BinaryTreeMaximumPathSum {
     int maxPathSum(TreeNode root);
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BinaryTreeMaximumPathSumImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BinaryTreeMaximumPathSumImpl.java
@@ -3,30 +3,37 @@ package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 import org.giddap.dreamfactory.leetcode.commons.TreeNode;
 import org.giddap.dreamfactory.leetcode.onlinejudge.BinaryTreeMaximumPathSum;
 
+/**
+ * Traverse the binary tree via DFS.
+ * At each node,
+ * * First decide locally,
+ * ** case i: left + node.val
+ * ** case ii: right + node.val
+ * ** case iii: node.val
+ * ** Update the max-path-sum, if needed.
+ * * Secondly, pass up to the parent node:
+ * ** case i: node.val
+ * ** case ii: node.val + max(left, right)
+ *
+ * A trick would be use max(0, left) and max(0, right) to ensure we don't
+ * count subtrees with negetive sums. This reduces the number of conditions
+ * to check.
+ */
 public class BinaryTreeMaximumPathSumImpl implements BinaryTreeMaximumPathSum {
-
     private int mps = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        // Initialize mps
-        mps = Integer.MIN_VALUE;
-        calculate(root);
+        mps = Math.max(helper(root), mps);
         return mps;
     }
 
-    private int calculate(TreeNode node) {
-        if (node == null) {
+    public int helper(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        // Calculate the case where MPS is generated at this node locally
-        int left = calculate(node.left);
-        int right = calculate(node.right);
-        int maxSumHere = node.val;
-        maxSumHere = Math.max(maxSumHere, maxSumHere + left);
-        maxSumHere = Math.max(maxSumHere, maxSumHere + right);
-        mps = Math.max(maxSumHere, mps);
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        mps = Math.max(left + root.val + right, mps);
 
-        // Return the max sum from the path passing through this node
-        // or this node and its max child
-        return Math.max(node.val, Math.max(left, right) + node.val);
+        return Math.max(root.val, root.val + Math.max(left, right));
     }
 }
