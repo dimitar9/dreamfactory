eac1748253cc9fec7ad9f426a048a293ade8a8a0
b1m4w3n <b1m4w3n@gmail.com>
2015-02-17 14:46:49 -0500

    leetcode - buy and sell stock

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BestTimeToBuyAndSellStock.java
@@ -1,18 +1,15 @@
 package org.giddap.dreamfactory.leetcode.onlinejudge;
 
 /**
- * <a href="http://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/">Best Time to Buy and Sell Stock</a>
+ * <a href="http://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/">
+ *     Best Time to Buy and Sell Stock</a>
  * <p/>
- * Copyright 2013 LeetCode
+ * Say you have an array for which the ith element is the price of a given
+ * stock on day i.
  * <p/>
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * <p/>
- * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
+ * If you were only permitted to complete at most one transaction
+ * (ie, buy one and sell one share of the stock),
  * design an algorithm to find the maximum profit.
- * <p/>
- *
- * @see <a href="http://leetcode.com/2010/11/best-time-to-buy-and-sell-stock.html">Leetcode blog</a>
- * http://n00tc0d3r.blogspot.com/2013/01/best-time-to-buy-and-sell-stock.html?q=best+time+to+buy+and+sell
  */
 public interface BestTimeToBuyAndSellStock {
     int maxProfit(int[] prices);
