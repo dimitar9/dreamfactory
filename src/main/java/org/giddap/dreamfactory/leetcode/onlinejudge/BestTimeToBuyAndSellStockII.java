257670bdbc09638a2073d37331e0c671f3f8505c
b1m4w3n <b1m4w3n@gmail.com>
2015-02-17 14:53:59 -0500

    leetcode - buy and sell stock

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BestTimeToBuyAndSellStockII.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/BestTimeToBuyAndSellStockII.java
@@ -1,17 +1,16 @@
 package org.giddap.dreamfactory.leetcode.onlinejudge;
 
 /**
- * <a href="http://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/">Best Time To Buy And Sell Stock II</a>
+ * <a href="http://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/">
+ *     Best Time To Buy And Sell Stock II</a>
  * <p/>
- * Copyright 2013 LeetCode
+ * Say you have an array for which the ith element is the price of a given
+ * stock on day i.
  * <p/>
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * <p/>
- * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one
- * and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (ie, buy one and sell one share of the stock
+ * multiple times). However, you may not engage in multiple transactions at the
  * same time (ie, you must sell the stock before you buy again).
- * <p/>
- * http://n00tc0d3r.blogspot.com/2013/01/best-time-to-buy-and-sell-stock.html
  */
 public interface BestTimeToBuyAndSellStockII {
     int maxProfit(int[] prices);
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BestTimeToBuyAndSellStockIIImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BestTimeToBuyAndSellStockIIImpl.java
@@ -2,13 +2,21 @@ package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 
 import org.giddap.dreamfactory.leetcode.onlinejudge.BestTimeToBuyAndSellStockII;
 
-public class BestTimeToBuyAndSellStockIIImpl implements BestTimeToBuyAndSellStockII {
+/**
+ * Greedy approach:
+ * Accumulate the positive incrementals.
+ */
+public class BestTimeToBuyAndSellStockIIImpl
+        implements BestTimeToBuyAndSellStockII {
     @Override
     public int maxProfit(int[] prices) {
-        int maxSum = 0;
-        for (int i = 1; i < prices.length; i++) {
-            maxSum += Math.max(0, prices[i] - prices[i - 1]);
+        final int n = prices.length;
+        int mp = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                mp += (prices[i] - prices[i - 1]);
+            }
         }
-        return maxSum;
+        return mp;
     }
 }
