d563371783383d358f2bc35278d8b34c8b8d4831
b1m4w3n <b1m4w3n@gmail.com>
2015-02-17 14:43:51 -0500

    leetcode - buy and sell stock

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BestTimeToBuyAndSellStockImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/BestTimeToBuyAndSellStockImpl.java
@@ -1,23 +1,28 @@
 package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 
-
 import org.giddap.dreamfactory.leetcode.onlinejudge.BestTimeToBuyAndSellStock;
 
-public class BestTimeToBuyAndSellStockImpl implements BestTimeToBuyAndSellStock {
+/**
+ * Time: O(n); Space: O(1)
+ * Iterate through the array while keeping two numbers:
+ * on ith element:
+ * * min_so_far: min(prices[i - 1], min_so_far)
+ * * max_profit_so_far: max(prices[i] - min_so_far, max_profit_so_far)
+ */
+public class BestTimeToBuyAndSellStockImpl
+        implements BestTimeToBuyAndSellStock {
     @Override
     public int maxProfit(int[] prices) {
-        final int len = prices.length;
-        if (len <= 1) {
+        final int n = prices.length;
+        if (n < 2) {
             return 0;
         }
-        int leftMin = prices[0];
-        int maxProfit = 0;
-
-        for (int i = 1; i < len; i++) {
-            maxProfit = Math.max(maxProfit, prices[i] - leftMin);
-            leftMin = Math.min(leftMin, prices[i]);
+        int mp = 0;
+        int min = prices[0];
+        for (int i = 1; i < n; i++) {
+            min = Math.min(prices[i - 1], min);
+            mp = Math.max(prices[i] - min, mp);
         }
-
-        return maxProfit;
+        return mp;
     }
 }
