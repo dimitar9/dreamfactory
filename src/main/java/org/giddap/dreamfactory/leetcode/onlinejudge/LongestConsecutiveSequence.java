aa3c948a26df89746337ac6754bdc44477edbeb6
b1m4w3n <b1m4w3n@gmail.com>
2015-02-17 23:06:50 -0500

    leetcode - longest consecutive sequence

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/LongestConsecutiveSequence.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/LongestConsecutiveSequence.java
@@ -10,15 +10,6 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
  * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
  * <p/>
  * Your algorithm should run in O(n) complexity.
- * <p/>
- * "Disjoint set.  Union find"
- * Links:
- * http://discuss.leetcode.com/questions/1070/longest-consecutive-sequence
- * http://blog.sina.com.cn/s/blog_b9285de20101iqar.html
- * http://en.wikipedia.org/wiki/Disjoint-set_data_structure
- * http://codedojo.konstantin-gurnov.com/job-interview-questions-on-algorithms-prinseton/
- * http://chuansong.me/n/93910
- * http://www.geeksforgeeks.org/union-find-algorithm-set-2-union-by-rank/
  */
 public interface LongestConsecutiveSequence {
     int longestConsecutive(int[] num);
--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/LongestConsecutiveSequenceImpl.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/implementations/LongestConsecutiveSequenceImpl.java
@@ -3,33 +3,37 @@ package org.giddap.dreamfactory.leetcode.onlinejudge.implementations;
 import org.giddap.dreamfactory.leetcode.onlinejudge.LongestConsecutiveSequence;
 
 import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Time: O(n); Space: O(n)
+ * Leverage Union-Find algorithm.
+ */
 public class LongestConsecutiveSequenceImpl implements LongestConsecutiveSequence {
-
     @Override
     public int longestConsecutive(int[] num) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int max = 1;
-        for (int i : num) {
-            if (map.containsKey(i)) continue;
-            map.put(i, 1);
-            if (map.containsKey(i - 1)) {
-                max = Math.max(max, merge(map, i - 1, i));
-            }
-
-            if (map.containsKey(i + 1)) {
-                max = Math.max(max, merge(map, i, i + 1));
+        Map<Integer, Integer> m = new HashMap<>();
+        int lcs = 1;
+        for (int n : num) {
+            if (!m.containsKey(n)) {
+                m.put(n, n);
+                // as the ceiling of the union
+                if (m.containsKey(n - 1)) {
+                    int lower = m.get(n - 1);
+                    m.put(lower, n);
+                    m.put(n, lower);
+                    lcs = Math.max(n - lower + 1, lcs);
+                }
+                // as the bottom of the union
+                if (m.containsKey(n + 1)) {
+                    int upper = m.get(n + 1);
+                    int lower = m.get(n);
+                    m.put(upper, lower);
+                    m.put(lower, upper);
+                    lcs = Math.max(upper - lower + 1, lcs);
+                }
             }
         }
-        return max;
-    }
-
-    private int merge(HashMap<Integer, Integer> map, int left, int right) {
-        int upper = right + map.get(right) - 1;
-        int lower = left - map.get(left) + 1;
-        int len = upper - lower + 1;
-        map.put(upper, len);
-        map.put(lower, len);
-        return len;
+        return lcs;
     }
 }
