862e30974d2fa56fbd538c4a2915ff3cfbec58b7
b1m4w3n <b1m4w3n@gmail.com>
2015-03-04 00:13:34 -0500

    leetcode - single number

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SingleNumber.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SingleNumber.java
@@ -2,6 +2,10 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
 
 /**
  * http://oj.leetcode.com/problems/single-number/
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * <p>
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  */
 public interface SingleNumber {
     int singleNumber(int[] A);
