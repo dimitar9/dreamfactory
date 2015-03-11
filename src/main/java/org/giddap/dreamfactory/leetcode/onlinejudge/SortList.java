81e01e40816f07b9959db98bf52ee58dd09302b7
b1m4w3n <b1m4w3n@gmail.com>
2015-03-11 00:00:44 -0400

    leetcode - sort list

--- src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SortList.java
+++ src/main/java/org/giddap/dreamfactory/leetcode/onlinejudge/SortList.java
@@ -3,7 +3,9 @@ package org.giddap.dreamfactory.leetcode.onlinejudge;
 import org.giddap.dreamfactory.leetcode.commons.ListNode;
 
 /**
- *
+ * https://leetcode.com/problems/sort-list/
+ * <p/>
+ * Sort a linked list in O(n log n) time using constant space complexity.
  */
 public interface SortList {
     ListNode sortList(ListNode head);
